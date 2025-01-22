6b80fbfe1f7958cc54ad53ffc0a1b5f50305a38a
Sachin Shetkar <ssshetkar2001@gmail.com>
2025-01-22 15:42:22 +0530

    Print Array Code

---
+++ Arrays/Program01.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Program01{
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        System.out.println(Arrays.toString(arr));
+    }
+}
